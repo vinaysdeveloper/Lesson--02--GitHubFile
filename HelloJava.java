6739b04cbbb06c1cb5d198cbcc23e09bdc04c671
Ubuntu <vinay.sb20@gmail.com>
2019-12-01 15:05:46 +0000

    I am pushn files to git

---
+++ HelloJava.java
@@ -0,0 +1,6 @@
+public class HellowJava {
+
+public static void main(String[] args){
+ System.out.println("Hellw I am Jva Prgram.Thank you");
+}
+}
---
+++ index.js
@@ -0,0 +1,2 @@
+var texts=" I am your JavaScipt";
+console.log(texts);
